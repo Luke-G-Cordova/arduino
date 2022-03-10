2446a3d16fda76065b0a795fc7fad50f29dee551
Luke Cordova <lukethegreat000@gmail.com>
2022-03-09 19:12:03 -0800

    started working on gpio and fixed a front end problem with the countdown

--- pi_tests/server.js
+++ pi_tests/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+var time = 0;
+// var Gpio = require('onoff').Gpio;
+// var relay = new Gpio(4, 'out');
+
 const corsInit = {
     origin: 'http://localhost:5252',
     optionsSuccessStatus: 200,
@@ -15,13 +19,30 @@ app.use('/', express.static(__dirname + '/public'))
 app.get('/', (req, res) => {
     res.sendFile('./public/index.html');
 });
-app.post('/', (req, res) => {
+app.post('/', timeout, (req, res) => {
+    // if(relay.readSync() === 0){
+    //     relay.writeSync(1);
+    // }
+    console.log('start');
+    time = 5;
     res.json({
-        time_left: 5
+        time_left: time
     });
+    setTimeout(() => {
+        console.log('end');
+        time = 0;
+        // relay.writeSync(0);
+    }, 5000);
+    
 });
 
 const PORT = 5252;
 app.listen(PORT, () => {
     console.log(`Server started on port: ${PORT}`);
-})
+});
+
+function timeout(req, res, next) {
+    if(time===0){
+        next();
+    }
+}
